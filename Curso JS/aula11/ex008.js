985e8d9405de338ec35e4fb4154185140247407a
Arthur <74980776+arth-inacio@users.noreply.github.com>
2023-04-21 16:40:33 -0300

    att ex008 e ex009

--- Curso JS/aula11/ex008.js
+++ Curso JS/aula11/ex008.js
@@ -1,6 +1,9 @@
-var vel = 20.5
+var vel = 60.5
 console.log(`A velocidade do seu carro é ${vel} Km/h`)
 
 if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permitida. Multado!!`)
 }
+console.log("========================================")
+console.log("Diriga sempre usando cinto de segurança!")
+console.log("========================================")
