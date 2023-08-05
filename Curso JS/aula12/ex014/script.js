122d0d19e06bdd5066df8badbc7a75c8becf76ec
Arthur <74980776+arth-inacio@users.noreply.github.com>
2023-08-05 17:21:39 -0300

    feat: exercicio com imagens e horas do dia

--- Curso JS/aula12/ex014/script.js
+++ Curso JS/aula12/ex014/script.js
@@ -3,6 +3,17 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-
+    
     msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12){
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e8d19e'
+    }else if (hora >= 12 && hora < 18){
+        img.src = 'fototarde.png'
+        document.body.style.background = '#d88770'
+    }else if (hora > 18) {
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#14253e'
+    }
 }
